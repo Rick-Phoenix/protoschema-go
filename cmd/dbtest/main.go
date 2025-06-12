e7db1f2b5f11e3b7af79b5ebcda02d3e2924be47
Rick-Phoenix <180959305+Rick-Phoenix@users.noreply.github.com>
2025-06-12 14:52:05 +0200

    sqlc version

--- cmd/dbtest/main.go
+++ cmd/dbtest/main.go
@@ -3,89 +3,34 @@ package main
 import (
 	"context"
 	"database/sql"
+	_ "embed"
+	"encoding/json"
 	"fmt"
+	"log"
 
-	"github.com/uptrace/bun"
-	"github.com/uptrace/bun/dialect/sqlitedialect"
-	"github.com/uptrace/bun/driver/sqliteshim"
-	"github.com/uptrace/bun/extra/bundebug"
+	gofirst "github.com/Rick-Phoenix/gofirst/db/queries/gen"
+	_ "modernc.org/sqlite"
 )
 
-type Profile struct {
-	ID     int64 `bun:",pk,autoincrement"`
-	Lang   string
-	Active bool
-	UserID int64
-}
-
-// User has many profiles.
-type User struct {
-	ID       int64 `bun:",pk,autoincrement"`
-	Name     string
-	Profiles []*Profile `bun:"rel:has-many,join:id=user_id"`
-}
-
 func main() {
+	// 1. Open the database
+	database, err := sql.Open("sqlite", "db/database.sqlite3")
+	if err != nil {
+		log.Fatalf("Failed to open database: %v", err)
+	}
+	defer database.Close()
+
+	queries := gofirst.New(database)
 	ctx := context.Background()
 
-	sqldb, err := sql.Open(sqliteshim.ShimName, "file::memory:?cache=shared")
+	// 4. Fetch the user with all their posts using the view
+	userWithPosts, err := queries.GetUserWithPostsFromView(ctx)
 	if err != nil {
-		panic(err)
+		log.Fatalf("Failed to get user with posts from view: %v", err)
 	}
 
-	db := bun.NewDB(sqldb, sqlitedialect.New())
-	defer db.Close()
+	var data []gofirst.Post
+	err = json.Unmarshal([]byte(userWithPosts.Posts), &data)
 
-	db.AddQueryHook(bundebug.NewQueryHook(bundebug.WithVerbose(true)))
-
-	if err := createSchema(ctx, db); err != nil {
-		panic(err)
-	}
-
-	user := new(User)
-	if err := db.NewSelect().
-		Model(user).
-		Column("user.*").
-		Relation("Profiles", func(q *bun.SelectQuery) *bun.SelectQuery {
-			return q.Where("active IS TRUE")
-		}).
-		OrderExpr("user.id ASC").
-		Limit(1).
-		Scan(ctx); err != nil {
-		panic(err)
-	}
-	fmt.Println(user.ID, user.Name, user.Profiles[0], user.Profiles[1])
-	fmt.Printf("%+v", user.Profiles[0])
-	// Output: 1 user 1 &{1 en true 1} &{2 ru true 1}
-}
-
-func createSchema(ctx context.Context, db *bun.DB) error {
-	models := []any{
-		(*User)(nil),
-		(*Profile)(nil),
-	}
-	for _, model := range models {
-		if _, err := db.NewCreateTable().Model(model).Exec(ctx); err != nil {
-			return err
-		}
-	}
-
-	users := []*User{
-		{ID: 1, Name: "user 1"},
-		{ID: 2, Name: "user 2"},
-	}
-	if _, err := db.NewInsert().Model(&users).Exec(ctx); err != nil {
-		return err
-	}
-
-	profiles := []*Profile{
-		{ID: 1, Lang: "en", Active: true, UserID: 1},
-		{ID: 2, Lang: "ru", Active: true, UserID: 1},
-		{ID: 3, Lang: "md", Active: false, UserID: 1},
-	}
-	if _, err := db.NewInsert().Model(&profiles).Exec(ctx); err != nil {
-		return err
-	}
-
-	return nil
+	fmt.Printf("%+v", data)
 }
