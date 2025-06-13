29a9c188bf5c8c42eeb380d7ccace1608e96aa2d
Rick-Phoenix <180959305+Rick-Phoenix@users.noreply.github.com>
2025-06-13 15:29:28 +0200

    2nd draft of col builder

--- internal/schemabuilder/main.go
+++ internal/schemabuilder/main.go
@@ -1,8 +1,57 @@
 package schemabuilder
 
 import (
+	"fmt"
 	"reflect"
 )
+
+type TableData map[string]ColumnBuilder
+type TablesDataType map[string]TableData
+
+var TablesData = TablesDataType{
+	"User": TableData{
+		"Name":      StringCol().Required().MinLen(3).Requests("create").Responses("get", "create").Nullable(),
+		"Age":       Int64Col().Responses("get").Nullable(),
+		"Blob":      BytesCol().Requests("get"),
+		"CreatedAt": TimestampCol().Responses("get"),
+	},
+}
+
+func CreateProto(schemaPtr any) ([]Column, error) {
+	schemaType := reflect.TypeOf(schemaPtr).Elem()
+	schemaName := schemaType.Name()
+
+	var columns []Column
+	var schemaData TableData
+	var ok bool
+
+	if schemaData, ok = TablesData[schemaName]; !ok {
+		return nil, fmt.Errorf("Could not find the data for the schema %s", schemaName)
+	}
+
+	for i := range schemaType.NumField() {
+		field := schemaType.Field(i)
+		fieldName := field.Name
+
+		if !field.IsExported() {
+			fmt.Printf("Ignoring unexported column %s...\n", field.Name)
+			continue
+		}
+
+		var colInstance ColumnBuilder
+
+		if colInstance, ok = schemaData[fieldName]; !ok {
+			return nil, fmt.Errorf("Could not find the data for the column %s in the table %s", fieldName, schemaName)
+		}
+
+		colData := colInstance.Build()
+
+		columns = append(columns, colData)
+
+	}
+
+	return columns, nil
+}
 
 // The FINAL, CORRECTED version of our unwrapper.
 func UnwrapToPlainStruct(richSchemaPtr any) any {
