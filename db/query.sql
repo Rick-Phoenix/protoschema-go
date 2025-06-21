df8e649905ee3db9dd2168ee25d38a47aa797637
Rick-Phoenix <180959305+Rick-Phoenix@users.noreply.github.com>
2025-06-21 12:58:24 +0200

    Updated queries

--- db/query.sql
+++ db/query.sql
@@ -6,3 +6,6 @@ SELECT * FROM users WHERE id = ?;
 
 -- name: GetPostsFromUserId :many
 SELECT * FROM posts WHERE author_id = ?;
+
+-- name: CreateUser :one
+INSERT INTO users (name) VALUES (?) Returning * ;
