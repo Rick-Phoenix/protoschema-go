f4a54eb1e85a72e7e4341c8c63b94f1b2c2d631f
Rick-Phoenix <180959305+Rick-Phoenix@users.noreply.github.com>
2025-07-07 15:59:21 +0200

    Moving some things to shared package

--- main_test.go
+++ main_test.go
@@ -1,18 +1,20 @@
-package protoschema
+package protoschema_test
 
 import (
 	"database/sql"
 	"path"
 	"testing"
 
+	p "github.com/Rick-Phoenix/protoschema"
+	"github.com/Rick-Phoenix/protoschema/hooks"
 	"github.com/Rick-Phoenix/protoschema/test/db"
 	"github.com/labstack/gommon/log"
 	"github.com/stretchr/testify/assert"
 )
 
 var (
-	goMod        = "github.com/Rick-Phoenix/protoschema"
-	protoPackage = NewProtoPackage(ProtoPackageConfig{
+	goMod   = "github.com/Rick-Phoenix/protoschema"
+	TestPkg = p.NewProtoPackage(p.ProtoPackageConfig{
 		Name:      "myapp.v1",
 		ProtoRoot: "testdata/proto",
 		GoPackage: path.Join(goMod, "gen/myappv1"),
@@ -20,120 +22,124 @@ var (
 	})
 )
 
-var PostFile = protoPackage.NewFile(FileSchema{
-	Package: protoPackage,
+var PostFile = TestPkg.NewFile(p.FileSchema{
+	Package: TestPkg,
 	Name:    "post",
 })
 
-var PostService = PostFile.NewService(ServiceSchema{
+var PostService = PostFile.NewService(p.ServiceSchema{
 	Resource: "Post",
-	Handlers: HandlersMap{
+	Handlers: p.HandlersMap{
 		"GetPost": {
 			Request:  GetPostRequest,
 			Response: GetPostResponse,
 		},
 		"UpdatePost": {
 			Request:  UpdatePostRequest,
-			Response: Empty(),
+			Response: p.Empty(),
 		},
 	},
 })
 
-var PostSchema = PostFile.NewMessage(MessageSchema{
+var PostSchema = PostFile.NewMessage(p.MessageSchema{
 	Name: "Post",
-	Fields: FieldsMap{
-		1: Int64("id"),
-		2: Timestamp("created_at"),
-		3: Int64("author_id"),
-		4: String("title").MinLen(5).MaxLen(64).Required(),
-		5: String("content").Optional(),
-		6: Int64("subreddit_id"),
+	Fields: p.FieldsMap{
+		1: p.Int64("id"),
+		2: p.Timestamp("created_at"),
+		3: p.Int64("author_id"),
+		4: p.String("title").MinLen(5).MaxLen(64).Required(),
+		5: p.String("content").Optional(),
+		6: p.Int64("subreddit_id"),
 	},
 	ModelIgnore: []string{"content", "updated_at"},
 	Model:       &db.Post{},
 })
 
-var GetPostRequest = PostFile.NewMessage(MessageSchema{
+var GetPostRequest = PostFile.NewMessage(p.MessageSchema{
 	Name: "GetPostRequest",
-	Fields: FieldsMap{
+	Fields: p.FieldsMap{
 		1: PostSchema.GetField("id"),
 	},
 })
 
-var GetPostResponse = PostFile.NewMessage(MessageSchema{
+var GetPostResponse = PostFile.NewMessage(p.MessageSchema{
 	Name: "GetPostResponse",
-	Fields: FieldsMap{
-		1: MsgField("post", PostSchema),
+	Fields: p.FieldsMap{
+		1: p.MsgField("post", PostSchema),
 	},
 })
 
-var UpdatePostRequest = PostFile.NewMessage(MessageSchema{Name: "UpdatePostRequest", Fields: FieldsMap{
-	1: MsgField("post", PostSchema),
-	2: FieldMask("field_mask"),
+var UpdatePostRequest = PostFile.NewMessage(p.MessageSchema{Name: "UpdatePostRequest", Fields: p.FieldsMap{
+	1: p.MsgField("post", PostSchema),
+	2: p.FieldMask("field_mask"),
 }})
 
-var UserFile = protoPackage.NewFile(FileSchema{
+var UserFile = TestPkg.NewFile(p.FileSchema{
 	Name:    "user",
-	Package: protoPackage,
+	Package: TestPkg,
 })
 
-var UserSchema = UserFile.NewMessage(MessageSchema{
+var UserSchema = UserFile.NewMessage(p.MessageSchema{
 	Name: "User",
-	Fields: FieldsMap{
-		1: String("name").Required().MinLen(2).MaxLen(32),
-		2: Int64("id"),
-		3: Timestamp("created_at"),
-		4: Repeated("posts", MsgField("post", PostSchema)),
+	Fields: p.FieldsMap{
+		1: p.String("name").Required().MinLen(2).MaxLen(32),
+		2: p.Int64("id"),
+		3: p.Timestamp("created_at"),
+		4: p.Repeated("posts", p.MsgField("post", PostSchema)),
 	},
 	Model: &db.UserWithPosts{},
 })
 
-var GetUserRequest = UserFile.NewMessage(MessageSchema{
+var GetUserRequest = UserFile.NewMessage(p.MessageSchema{
 	Name: "GetUserRequest",
-	Fields: FieldsMap{
-		1: Int64("id").Required(),
+	Fields: p.FieldsMap{
+		1: p.Int64("id").Required(),
 	},
 })
 
-var GetUserResponse = UserFile.NewMessage(MessageSchema{
+var GetUserResponse = UserFile.NewMessage(p.MessageSchema{
 	Name: "GetUserResponse",
-	Fields: FieldsMap{
-		1: MsgField("user", UserSchema),
+	Fields: p.FieldsMap{
+		1: p.MsgField("user", UserSchema),
 	},
 })
 
-var UpdateUserRequest = UserFile.NewMessage(MessageSchema{
+var UpdateUserRequest = UserFile.NewMessage(p.MessageSchema{
 	Name: "UpdateUserRequest",
-	Fields: FieldsMap{
-		1: Int64("id").Required(),
-		2: FieldMask("field_mask"),
+	Fields: p.FieldsMap{
+		1: p.Int64("id").Required(),
+		2: p.FieldMask("field_mask"),
 	},
 })
 
-var UserService = UserFile.NewService(ServiceSchema{
+var UserService = UserFile.NewService(p.ServiceSchema{
 	Resource: "User",
-	Handlers: HandlersMap{
+	Handlers: p.HandlersMap{
 		"GetUser": {
 			Request:  GetUserRequest,
 			Response: GetUserResponse,
 		},
 		"UpdateUser": {
 			Request:  UpdateUserRequest,
-			Response: Empty(),
+			Response: p.Empty(),
 		},
 	},
 })
 
 func TestMain(t *testing.T) {
+	// assert.NoError(t, err, "Main Test")
+}
+
+func TestHandlerGen(t *testing.T) {
 	database, err := sql.Open("sqlite", ":memory:")
 	if err != nil {
 		log.Fatalf("Failed to open database: %v", err)
 	}
 	store := db.New(database)
-	handlerBuilder := NewHandlerBuilder(store, "gen/handlers")
-	files := protoPackage.BuildFiles()
+	handlerBuilder := hooks.NewHandlerBuilder(store, "gen/handlers")
+	files := TestPkg.BuildFiles()
 	for _, file := range files {
-		err := handlerBuilder.genConnectHandler(file)
-		assert.NoError(t, err, "Main Test")
+		err := handlerBuilder.Generate(file)
+		assert.NoError(t, err, "Gen handler test")
 	}
 }
